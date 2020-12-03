813eb3aeadf289cf1927e545dfdeeab60af6f444
Vidyadhar1997 <vidyadharhudge1997@gmail.com>
2020-12-03 16:24:43 +0530

    [vidyadhar hudge]Add:Add a new contact to adress book

---
+++ AdressBookSystem/AdressBookBuilder.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AdressBookSystem
+{
+    class AdressBookBuilder
+    {
+        public List<Contact> contactList;
+        public AdressBookBuilder()
+        {
+            this.contactList = new List<Contact>();
+        }
+        public void AddContact(String fName, String lName, String address, String city, String state, String zip, String phoneNumber, String email)
+        {
+            Contact contact = new Contact(fName,lName,address,city,state,zip,phoneNumber,email);
+            contactList.Add(contact);
+        }
+        public void displayContact()
+        {
+            foreach(Contact contact in contactList)
+            {
+                Console.WriteLine("\nFirst name = " + contact.fName);
+                Console.WriteLine("Last name = " + contact.lName);
+                Console.WriteLine("Address = " + contact.address);
+                Console.WriteLine("city = " + contact.city);
+                Console.WriteLine("state = " + contact.state);
+                Console.WriteLine("zip = " + contact.zip);
+                Console.WriteLine("phoneNumber = " + contact.phoneNumber);
+                Console.WriteLine("email = " + contact.email);
+
+            }
+        }
+    }
+}
--- AdressBookSystem/Program.cs
+++ AdressBookSystem/Program.cs
@@ -8,6 +8,29 @@ namespace AdressBookSystem
         {
             Console.WriteLine("Welcome To adress Book System!");
             Program program = new Program();
+            Console.WriteLine("Enter first name = ");
+            string fName = Console.ReadLine();
+            Console.WriteLine("Enter last name = ");
+            string lName = Console.ReadLine();
+            Console.WriteLine("Enter address= ");
+            String address=Console.ReadLine();
+            Console.WriteLine("Enter city= ");
+            String city = Console.ReadLine();
+            Console.WriteLine("Enter state= ");
+            String state = Console.ReadLine();
+            Console.WriteLine("Enter zip= ");
+            String zip = Console.ReadLine();
+            Console.WriteLine("Enter phoneNumber= ");
+            String phoneNumber = Console.ReadLine();
+            Console.WriteLine("Enter email= ");
+            String email = Console.ReadLine();
+
+            AdressBookBuilder adressBookBuilder = new AdressBookBuilder();
+            adressBookBuilder.AddContact(fName, lName, address, city, state, zip, phoneNumber, email);
+            adressBookBuilder.displayContact();
+
+
+
         }
     }
 }
