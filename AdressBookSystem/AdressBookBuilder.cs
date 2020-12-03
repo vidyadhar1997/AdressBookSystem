977cf24a6b37accc23605c284df3f61eced91b69
Vidyadhar1997 <vidyadharhudge1997@gmail.com>
2020-12-03 17:18:57 +0530

    [vidyadhar hudge]Add:Edit existing contact with help of name

--- AdressBookSystem/AdressBookBuilder.cs
+++ AdressBookSystem/AdressBookBuilder.cs
@@ -11,10 +11,40 @@ namespace AdressBookSystem
         {
             this.contactList = new List<Contact>();
         }
-        public void AddContact(String fName, String lName, String address, String city, String state, String zip, String phoneNumber, String email)
+        public void addContact(String fName, String lName, String address, String city, String state, String zip, String phoneNumber, String email)
         {
             Contact contact = new Contact(fName,lName,address,city,state,zip,phoneNumber,email);
             contactList.Add(contact);
+        }
+        public void editContact(string fName)
+        {
+            int flag = 1;
+            foreach (Contact contact in contactList)
+            {
+                if (fName.Equals(contact.fName))
+                {
+                    flag = 0;
+                    Console.WriteLine("Enter last name = ");
+                    string lName = Console.ReadLine();
+                    Console.WriteLine("Enter address= ");
+                    String address = Console.ReadLine();
+                    Console.WriteLine("Enter city= ");
+                    String city = Console.ReadLine();
+                    Console.WriteLine("Enter state= ");
+                    String state = Console.ReadLine();
+                    Console.WriteLine("Enter zip= ");
+                    String zip = Console.ReadLine();
+                    Console.WriteLine("Enter phoneNumber= ");
+                    String phoneNumber = Console.ReadLine();
+                    Console.WriteLine("Enter email= ");
+                    String email = Console.ReadLine();
+                }
+
+            }
+            if (flag == 1)
+            {
+                Console.WriteLine("Contact not found");
+            }
         }
         public void displayContact()
         {
--- AdressBookSystem/Program.cs
+++ AdressBookSystem/Program.cs
@@ -6,31 +6,49 @@ namespace AdressBookSystem
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome To adress Book System!");
-            Program program = new Program();
-            Console.WriteLine("Enter first name = ");
-            string fName = Console.ReadLine();
-            Console.WriteLine("Enter last name = ");
-            string lName = Console.ReadLine();
-            Console.WriteLine("Enter address= ");
-            String address=Console.ReadLine();
-            Console.WriteLine("Enter city= ");
-            String city = Console.ReadLine();
-            Console.WriteLine("Enter state= ");
-            String state = Console.ReadLine();
-            Console.WriteLine("Enter zip= ");
-            String zip = Console.ReadLine();
-            Console.WriteLine("Enter phoneNumber= ");
-            String phoneNumber = Console.ReadLine();
-            Console.WriteLine("Enter email= ");
-            String email = Console.ReadLine();
-
             AdressBookBuilder adressBookBuilder = new AdressBookBuilder();
-            adressBookBuilder.AddContact(fName, lName, address, city, state, zip, phoneNumber, email);
-            adressBookBuilder.displayContact();
-
-
-
+            while (true)
+            {
+                Console.WriteLine("\n 1 for Add Contact \n 2 for Edit Existing Contact \n 3 for exit ");
+                int choise = Convert.ToInt32(Console.ReadLine());
+                switch (choise)
+                {
+                    case 1:
+                        Console.WriteLine("Welcome To  Book System!");
+                        Program program = new Program();
+                        Console.WriteLine("Enter first name = ");
+                        string fName = Console.ReadLine();
+                        Console.WriteLine("Enter last name = ");
+                        string lName = Console.ReadLine();
+                        Console.WriteLine("Enter address= ");
+                        String address = Console.ReadLine();
+                        Console.WriteLine("Enter city= ");
+                        String city = Console.ReadLine();
+                        Console.WriteLine("Enter state= ");
+                        String state = Console.ReadLine();
+                        Console.WriteLine("Enter zip= ");
+                        String zip = Console.ReadLine();
+                        Console.WriteLine("Enter phoneNumber= ");
+                        String phoneNumber = Console.ReadLine();
+                        Console.WriteLine("Enter email= ");
+                        String email = Console.ReadLine();
+                        adressBookBuilder.addContact(fName, lName, address, city, state, zip, phoneNumber, email);
+                        adressBookBuilder.displayContact();
+                        break;
+                    case 2:
+                        Console.WriteLine("Enter first name = ");
+                        string seachFirstName = Console.ReadLine();
+                        adressBookBuilder.editContact(seachFirstName);
+                        adressBookBuilder.displayContact();
+                        break;
+                    case 3:
+                        Environment.Exit(0);
+                        break;
+                    default:
+                        Console.WriteLine("Enter The Valid Choise");
+                        break;
+                }
+            }
         }
     }
 }
